e9fc8f903a0688ed0e966a6169b94ba9797dddc2
Ankit-Dwivedi-code <152896806+Ankit-Dwivedi-code@users.noreply.github.com>
2024-07-03 18:25:15 +0530

    Add newq routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {  changeCurrentPassword, getCurrentUser, loginUser, logoutUser, registerUser, renewRefreshToken, updateUserAvatar, updateUserCoverImage, updateUserDetails } from "../controllers/user.controller.js";
+import {  changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, registerUser, renewRefreshToken, updateUserAvatar, updateUserCoverImage, updateUserDetails } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -28,8 +28,10 @@ router.route("/logout").post(verifyJWT ,logoutUser)
 router.route("/refresh-token").post(renewRefreshToken)
 router.route("/update-password").post(verifyJWT, changeCurrentPassword)
 router.route("/getcurrentuser").post(verifyJWT, getCurrentUser)
-router.route("/update-details").post(verifyJWT, updateUserDetails)
-router.route("/update-avatar").post(upload.single("avatar"), verifyJWT, updateUserAvatar)
-router.route("/update-coverimage").post(upload.single("coverImage"), verifyJWT, updateUserCoverImage)
+router.route("/update-details").patch(verifyJWT, updateUserDetails)
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar)
+router.route("/update-coverimage").patch(verifyJWT, upload.single("coverImage"),  updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 
 export default router
